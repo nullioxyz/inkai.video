7d7999d721d0ce06bb627b86903d7413923a2f9b
Rafael Franke <frankerafael@bikeflip.com>
2026-02-16 22:56:12 +0100

    feat: landing

--- src/components/shared/AppShell.tsx
+++ src/components/shared/AppShell.tsx
@@ -4,6 +4,7 @@ import { ReactNode, Suspense } from 'react';
 import { usePathname } from 'next/navigation';
 import HeaderWrapper from '@/components/shared/HeaderWrapper';
 import SmoothScrollProvider from '@/components/shared/SmoothScroll';
+import DemoShowcase from '@/components/shared/demo-showcase';
 import Footer from '@/components/shared/footer/Footer';
 
 interface AppShellProps {
@@ -18,6 +19,7 @@ const AppShell = ({ children }: AppShellProps) => {
     <Suspense>
       <SmoothScrollProvider>
         {!isDashboardRoute && <HeaderWrapper />}
+        {!isDashboardRoute && <DemoShowcase activeDemoId={1} />}
         {children}
         {!isDashboardRoute && <Footer />}
       </SmoothScrollProvider>
